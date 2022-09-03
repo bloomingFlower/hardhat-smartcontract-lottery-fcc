9c3968c405d0bc478f0756ba782f3d69e4eab800
AAII <sinsunby@gmail.com>
2022-09-04 03:11:24 +0900

    152031

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,12 +1,22 @@
 const { developmentChains } = require("../helper-hardhat-config")
 
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is premium. It consts 0.25 LINK
+const GAS_PRICE_LINK = 1e9 // link per gas.
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
         //deploy a mock vrfcoordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mock Deployed")
+        log("--------------------------------------------------")
     }
 }
